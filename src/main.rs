2805afab70c5f1dd5c6da34947d3636e0a04834c
DSOM <lemlu@protonmail.com>
2019-12-09 18:57:18 +0100

    implemented pretty tree printing for easier debugging

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,11 @@ extern crate pest;
 extern crate pest_derive;
 
 use std::env;
+use std::fmt::Debug;
+use std::ops::Deref;
 use std::path::{Path, PathBuf};
+
+use indextree::{Arena, NodeId};
 
 mod frontend;
 
@@ -15,8 +19,29 @@ fn main() {
     path.push(Path::new("test.v"));
     match { frontend::run_frontend(path.as_path()) } {
         Ok(ast) => {
-            println!("{}", ast.0.get(ast.1).unwrap())
+            pprint_tree(ast.1, &ast.0)
         }
         Err(E) => println!("{}", E)
     }
+}
+
+fn pprint_tree<T: Debug>(top_node: NodeId, arena: &Arena<T>) {
+    fn pprint_tree<T: Debug>(node: NodeId, arena: &Arena<T>, prefix: String, last: bool) {
+        let prefix_current = if last { "`- " } else { "|- " };
+
+        println!("{}{}{:?}", prefix, prefix_current, arena.get(node).unwrap().get());
+
+        let prefix_child = if last { "   " } else { "|  " };
+        let prefix = prefix + prefix_child;
+
+        if node.children(arena).next().is_some() {
+            let last_child = node.children(arena).count() - 1;
+
+            for (i, child) in node.children(arena).enumerate() {
+                pprint_tree(child, arena, prefix.to_string(), i == last_child);
+            }
+        }
+    }
+
+    pprint_tree(top_node, arena, "".to_string(), true);
 }
