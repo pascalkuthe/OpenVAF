ed6b11a2a885d7f32c969faf47f842cf767ca585
DSPOM <3189098-jamescoding@users.noreply.gitlab.com>
2019-12-15 23:00:56 +0100

    Added some basic tet
    Signed-off-by: jarodcoding <jarodkuthe@gmail.com>

--- src/frontend/preprocessor.rs
+++ src/frontend/preprocessor.rs
@@ -8,12 +8,6 @@
  * *****************************************************************************************
  */
 
-//fn gpair: Pair<Rule>) {
-//    panic!("Unexpected Rule {:?} from string {}", pair.as_rule(), pair.as_str());
-//}
-
-
-//Preprocessor and Parse tree generation
 
 use std::{fs, mem};
 use std::cell::{RefCell, RefMut};
@@ -299,5 +293,64 @@ impl<'lt> Preprocessor<'lt> {
     }
 }
 
+#[cfg(test)]
+mod test{
+    use std::fs;
+    use crate::setup_logger;
+    use crate::frontend::parser::create_parse_tree;
+    use super::*;
+    use pest::error::{Error, ErrorVariant};
+    use pest::RuleType;
 
+    #[test]
+    pub fn macro_test(){
+        setup_logger();
+        let file_contents = fs::read_to_string("tests/macros.va").expect("File not found!");
+        let mut preprocessor = Preprocessor::new();
+        if let Err(e) = preprocessor.run_preprocessor(&file_contents){
+            panic!();
+        }
+        let preprocessed_source = preprocessor.finalize();
+        //Trimming is done here since amount of newlines may change and mostly doesnt make any statement about correctness (as long as necessary ones arent removed)
+        //TODO seperate test for trimming?
+        assert_eq!(preprocessed_source.replace("\n","").replace("\r",""),"OK1, OK2,__SMS__OK3OK3L,OK4")
+    }
+
+    pub fn code_invariance(){
+        setup_logger();
+        let file_contents = fs::read_to_string("tests/invariance.va").expect("File not found!");
+        let mut preprocessor = Preprocessor::new();
+        if let Err(e) = preprocessor.run_preprocessor(&file_contents){
+            panic!();
+        }
+        let preprocessed_source = preprocessor.finalize();
+        let parse_tree = match create_parse_tree(&preprocessed_source){
+            Ok(result) => result,
+            Err(e)=>panic!()
+        };
+        let control_source = fs::read_to_string("tests/invariance_control.va").expect("File not found!");
+        let control_parse_tree = match create_parse_tree(&control_source){
+            Ok(result) => result,
+            Err(e)=>panic!()
+        };
+        if let Err(e) = assert_eq_parse_tree(parse_tree,control_parse_tree){
+            panic!(e)
+        }
+    }
+    fn assert_eq_parse_tree<T:RuleType>(top_node1: Pair<T>,top_node2: Pair<T>) -> std::result::Result<(),String>{
+        if top_node1.as_rule()!=top_node2.as_rule() {
+            return Err(format!("Rules mismatch for {:?},{:?}",top_node1,top_node2));
+        }
+        let mut children_tree1 = top_node1.clone().into_inner();
+        let mut children_tree2 = top_node2.clone().into_inner();
+        while let (Some(child1),Some(child2)) = (children_tree1.next(),children_tree2.next()){
+            assert_eq_parse_tree(child1,child2)?
+        }
+        if children_tree1.next().is_none()&&children_tree2.next().is_none(){
+            Ok(())
+        }else{
+            return Err(format!("Children mismatch for {:?},{:?}",top_node1,top_node2));
+        }
+    }
+}
 
