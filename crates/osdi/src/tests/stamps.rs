c48932eea7a265641cb8cac8a0a19fcc88a94bed
Markus Mueller <metroid120@gmail.com>
2022-03-10 13:25:50 +0100

    test(osdi): added test for CS, CCCS and amplifier

--- crates/osdi/src/tests/stamps.rs
+++ crates/osdi/src/tests/stamps.rs
@@ -61,6 +61,133 @@ fn resistor() {
 
 }
 
+#[test]
+fn current_source() {
+    // compile model and obtain:
+    // a data base that contains all info about the model available to the compiler
+    // the optimized MIR that represents the actual compiled code (including matrix entries)
+    // the interned string literals (unintersting)
+    let root_file = project_root().join("integration_tests").join("CURRENT_SOURCE").join("current_source.va");
+    let (db, mir, mut literals) = super::compile_to_mir(&root_file);
+
+    //define parameters
+    let i = 1.0;
+    let r = 1e3;
+
+    // prepare inputs
+    let temp = 298.5;
+    let mut params = AHashMap::default();
+    let mut node_voltages = AHashMap::default();
+
+    params.insert("Io", i.into());
+    params.insert("R", r.into());
+
+    node_voltages.insert("Np", 1.0);
+    node_voltages.insert("Nm", 0.0);
+
+    // run the interpreter
+    let result = mir.interpret(&db, &mut literals, &params, &node_voltages, temp);
+    let stamps = mir.matrix.stamps(&db);
+
+    // read the matrix entries
+    let ip_vp : f64  = result.read(stamps[&("Np".to_owned(),"Np".to_owned())]);
+    let ip_vm : f64  = result.read(stamps[&("Np".to_owned(),"Nm".to_owned())]);
+    let im_vp : f64  = result.read(stamps[&("Nm".to_owned(),"Np".to_owned())]);
+    let im_vm : f64  = result.read(stamps[&("Nm".to_owned(),"Nm".to_owned())]);
+
+    // calculate the expected values for the stamps
+    let g = 1.0/r;
+
+    // Resistor current flows from A into B, resistor voltage = Va-Vb
+    let ip_vp_expect = g;
+    let ip_vm_expect = -g;
+    let im_vm_expect = g;
+    let im_vp_expect = -g;
+
+    // finally assert that the values are correct
+    let epsilon=1e-5;
+    assert_approx_eq!(f64, ip_vp, ip_vp_expect, epsilon=epsilon);
+    assert_approx_eq!(f64, ip_vm, ip_vm_expect, epsilon=epsilon);
+    assert_approx_eq!(f64, im_vm, im_vm_expect, epsilon=epsilon);
+    assert_approx_eq!(f64, im_vp, im_vp_expect, epsilon=epsilon);
+
+}
+
+#[test]
+fn cccs() {
+    // compile model and obtain:
+    // a data base that contains all info about the model available to the compiler
+    // the optimized MIR that represents the actual compiled code (including matrix entries)
+    // the interned string literals (unintersting)
+    let root_file = project_root().join("integration_tests").join("CCCS").join("cccs.va");
+    let (db, mir, mut literals) = super::compile_to_mir(&root_file);
+
+    //print assembly
+    // println!("{}", mir.func.to_debug_string());
+
+    //define parameters
+    let rin = 1.0;
+    let rout = 1e9;
+    let g = 1e2;
+
+    // prepare inputs
+    let temp = 298.5;
+    let mut params = AHashMap::default();
+    let mut node_voltages = AHashMap::default();
+
+    params.insert("G", g.into());
+    params.insert("Rin", rin.into());
+    params.insert("Rout", rout.into());
+
+    node_voltages.insert("Inp", 1.0);
+    node_voltages.insert("Inm", 0.0);
+    node_voltages.insert("Outp", 1e-3);
+    node_voltages.insert("Outm", 0.0);
+
+    // run the interpreter
+    let result = mir.interpret(&db, &mut literals, &params, &node_voltages, temp);
+    let stamps = mir.matrix.stamps(&db);
+
+    // read the matrix entries
+    let op_op : f64  = result.read(stamps[&("Outp".to_owned(),"Outp".to_owned())]);
+    let op_om : f64  = result.read(stamps[&("Outp".to_owned(),"Outm".to_owned())]);
+    let om_op : f64  = result.read(stamps[&("Outm".to_owned(),"Outp".to_owned())]);
+    let om_om : f64  = result.read(stamps[&("Outm".to_owned(),"Outm".to_owned())]);
+    let ip_ip : f64  = result.read(stamps[&("Inp".to_owned(),"Inp".to_owned())]);
+    let ip_im : f64  = result.read(stamps[&("Inp".to_owned(),"Inm".to_owned())]);
+    let im_ip : f64  = result.read(stamps[&("Inm".to_owned(),"Inp".to_owned())]);
+    let im_im : f64  = result.read(stamps[&("Inm".to_owned(),"Inm".to_owned())]);
+
+
+    // calculate the expected values for the stamps
+    let gin = 1.0/rin;
+    let gout = 1.0/rout;
+
+    let ip_ip_expect = gin;
+    let ip_im_expect = -gin;
+    let im_ip_expect = -gin;
+    let im_im_expect = gin;
+    let op_op_expect = gout;
+    let op_om_expect = -gout;
+    let om_op_expect = -gout;
+    let om_om_expect = gout;
+
+
+    // finally assert that the values are correct
+    let epsilon=1e-5;
+    assert_approx_eq!(f64, ip_ip, ip_ip_expect, epsilon=epsilon);
+    assert_approx_eq!(f64, ip_im, ip_im_expect, epsilon=epsilon);
+    assert_approx_eq!(f64, im_ip, im_ip_expect, epsilon=epsilon);
+    assert_approx_eq!(f64, im_im, im_im_expect, epsilon=epsilon);
+    assert_approx_eq!(f64, op_op, op_op_expect, epsilon=epsilon);
+    assert_approx_eq!(f64, op_om, op_om_expect, epsilon=epsilon);
+    assert_approx_eq!(f64, om_op, om_op_expect, epsilon=epsilon);
+    assert_approx_eq!(f64, om_om, om_om_expect, epsilon=epsilon);
+
+}
+
+
+
 
 #[test]
 fn diode() {
