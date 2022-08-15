2f397fe9812c89bfd46ea706cbd7fe4286cc4f31
DSPOM <dspom@protonmail.com>
2022-08-15 13:57:52 +0200

    chore(ci): disable calls to clang/llvm-config when RUST_CHECK is set
    When running clippy we do not care about actually calling
    clang and linking to llvm in build scripts. Disabeling these build scripts
    allows us to run clippy in a much smaller container without clang/llvm-devel
    (which means faster CI times).
    
    This commit adds conditions to the osdi and llvm build scripts that
    disable linking/calls to clang when RUST_CHECK is set

--- crates/osdi/build.rs
+++ crates/osdi/build.rs
@@ -1,9 +1,22 @@
 use std::env;
+use std::ffi::{OsStr, OsString};
+use std::fmt::Display;
 use std::path::Path;
 
 use xshell::{cmd, Shell};
 
+/// Reads an environment variable and adds it to dependencies.
+/// Supposed to be used for all variables except those set for build scripts by cargo
+/// <https://doc.rust-lang.org/cargo/reference/environment-variables.html#environment-variables-cargo-sets-for-build-scripts>
+fn tracked_env_var_os<K: AsRef<OsStr> + Display>(key: K) -> Option<OsString> {
+    println!("cargo:rerun-if-env-changed={}", key);
+    env::var_os(key)
+}
+
 fn main() {
+    // If we're just running `check`, there's no need to actually compute the stdlib just
+    // popualte dummys
+    let no_gen = tracked_env_var_os("RUST_CHECK").is_some();
     let sh = Shell::new().unwrap();
     let osdi_dir = sourcegen::project_root().join("crates").join("osdi");
     let src_file = osdi_dir.join("stdlib.c");
@@ -25,10 +38,14 @@ fn main() {
 
         let out_dir = env::var_os("OUT_DIR").unwrap();
         let out_file = Path::new(&out_dir).join(&format!("stdlib_{version_str}.bc"));
-        println!("cargo:rerun-if-changed={}", file.display());
-        cmd!(sh, "clang -emit-llvm -O0 -D{def_name} -o {out_file} -c {src_file} -fPIC")
-            .run()
-            .expect("failed to generate bitcode");
+        if no_gen {
+            sh.write_file(out_file, &[]).expect("failed to write dummy file");
+        } else {
+            println!("cargo:rerun-if-changed={}", file.display());
+            cmd!(sh, "clang -emit-llvm -O0 -D{def_name} -o {out_file} -c {src_file} -fPIC")
+                .run()
+                .expect("failed to generate bitcode");
+        }
     }
 
     println!("cargo:rerun-if-changed={}", src_file.display());
